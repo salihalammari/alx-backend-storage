022b3b643c7aa6551790d902b9bdb50d27cc2895
salihalammari <slammari@student.1337.ma>
2024-05-26 22:30:13 +0000

    add radis

---
+++ 0x02-redis_basic/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
+
+bob@dylan:~$ python3 main.py 
+3a3e8231-b2f6-450d-8b0e-0f38f16e8ca2
+b'hello'
