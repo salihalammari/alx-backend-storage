621b0edb9653d3118eaa34ed7a78997e4dd01a79
root <slammari@student.1337.ma>
2024-07-29 17:40:34 +0000

    add incr

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,18 +4,21 @@
 import redis
 import requests
 r = redis.Redis()
-count = 0
 
 
 def get_page(url: str) -> str:
     """ track how many times a particular URL was accessed in the key
         "count:{url}"
         and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
+    cached_content = r.get(f"cached:{url}")
+    if cached_content:
+        return cached_content.decode('utf-8')
     resp = requests.get(url)
+    page_content = resp.text
+    r.setex(f"cached:{url}", 10, page_content)
     r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
+
+    return page_content
 
 
 if __name__ == "__main__":
